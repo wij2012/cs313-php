0c7b1c3bd1285c3e9e161a80486e818dd08ed0d4
William Johnson <joh15132@byui.edu>
2018-05-03 16:50:05 -0600

    link highlighted

--- web/TeamActivity02/navbar.php
+++ web/TeamActivity02/navbar.php
@@ -1,7 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="navbar"> 
+        <ul> 
+        <li class="<?php if($page == 'login') {echo 'active';}?>"> <a href="login.php"> Login </a> </li>
+        <li class="<?php if($page == 'home') {echo 'active';}?>"><a href="home.php"> Home </a> </li> 
+        <li class="<?php if($page == 'about') {echo 'active';}?>"> <a href="about-us.php"> About Us </a> </li> 
+        </ul> 
+    </div>
+</body>
+</html>
 
-<?php
-echo '<div class="navbar"> 
-<a href="login.php"> Login </a>
-<a href="home.php"> Home </a> 
-<a href="about-us.php"> About Us </a> </div>';
-?>
