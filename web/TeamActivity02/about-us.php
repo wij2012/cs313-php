7dd0ddd2d01c8a8da9efdefce343cb2d44f69028
William Johnson <joh15132@byui.edu>
2018-05-03 16:53:24 -0600

    NOW link highlighted

--- web/TeamActivity02/about-us.php
+++ web/TeamActivity02/about-us.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php 
+    $page = 'about';
+    include 'navbar.php';
+    ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -10,9 +14,6 @@
 <body>
     <h1>Donuts inc.</h1>
     
-    <?php
-        include 'navbar.php';
-    ?>
     
     <p>We eat and deliver donuts right to your door! <br> Make your orders on our website or give us a call. </p>
 </body>
