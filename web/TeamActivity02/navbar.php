0fadc576d0918fb01eb7a990f35e6e3db1959508
William Johnson <joh15132@byui.edu>
2018-05-03 16:33:06 -0600

    php error corrected

--- web/TeamActivity02/navbar.php
+++ web/TeamActivity02/navbar.php
@@ -3,5 +3,5 @@
 echo '<div class="navbar"> 
 <a href="login.php"> Login </a>
 <a href="home.php"> Home </a> 
-<a href="about-us.php"> About Us </a></div>;
+<a href="about-us.php"> About Us </a> </div>';
 ?>
