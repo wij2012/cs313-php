c1f1babad28ccab8def77ebc495d5358c366c67b
William Johnson <joh15132@byui.edu>
2018-05-11 13:29:50 -0600

    debugging script continued

--- web/ShoppingCart/shopjavascript.js
+++ web/ShoppingCart/shopjavascript.js
@@ -1,6 +1,6 @@
 function onToNumbers(){
     if(document.getElementById("ironman1").innerHTML == true){
         document.getElementById("ironman1").value = 1;
-        document.getElementById("testing").value = document.getElementById("ironman1").value; 
+        document.getElementById("testing").innerHTML = document.getElementById("ironman1").value; 
     }
 }
