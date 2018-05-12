9c635b334805ce55a8eca888539d095421af11db
William Johnson <joh15132@byui.edu>
2018-05-12 11:23:10 -0600

    php stuff

---
+++ web/ShoppingCart/printCart.php
@@ -0,0 +1,28 @@
+<?php
+
+include 'makeCart.php';
+
+echo "<h1> Your shopping cart </h1>";
+echo "Iron Man: $ironman1 <br>";
+echo "Iron Man 2: $ironman2 <br>";
+echo "Iron Man 3: $ironman3 <br>";
+echo "Captain America: The First Avenger: $cap1 <br>";
+echo "Captain America: The Winter Soldier: $cap2 <br>";
+echo "Captain America: Civil War: $cap3 <br>";
+echo "Thor: $thor1 <br>";
+echo "Thor: The Dark World: $thor2 <br>";
+echo "Thor: Ragnarok: $thor3 <br>";
+echo "Avengers: $avengers1 <br>";
+echo "Avengers: Age of Ultron: $avengers2 <br>";
+echo "Spider-Man: Homecoming: $spider <br>";
+echo "Guardians of the Galaxy: $guardians1 <br>";
+echo "Guardians of the Galaxy: Volume 2: $guardians2 <br>";
+echo "Black Panther: $panther <br>";
+echo "THe Incredible Hulk: $hulk <br>";
+echo "Ant-Man: $ant <br>";
+
+//echo "<table> <tr> <th> Item </th> <th> Number in cart </th> </tr>";
+//echo "<tr> <td> Iron Man </td> <td>" . $cart['ironman'] . "</td></tr>";
+
+//echo "</table>"
+?>
