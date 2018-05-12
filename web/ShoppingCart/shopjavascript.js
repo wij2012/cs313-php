3a18a0968ab377e5c3036a883cee50c871b61d42
William Johnson <joh15132@byui.edu>
2018-05-11 19:31:49 -0600

    working on it

--- web/ShoppingCart/shopjavascript.js
+++ web/ShoppingCart/shopjavascript.js
@@ -4,11 +4,7 @@ function onToNumbers(){
         //document.getElementById("ironman1").value = 1;
         //document.getElementById("testing").innerHTML = document.getElementById("ironman1").value; 
 
-        //if(document.getElementById("preformance")[x].value == "Duet"){
-            document.getElementById("i1howmany").innerHTML = "Input the amount of this item you wish to purchase. <input type='text' id='numiron1'>";
-            document.getElementById("i1howmany").innerHTML += "<br> <button value='Add this many to your cart'>";
-            //First Name - Second Student <br> <input type='text' id='sfName' name='sfName'>";
-            
-            //}
+        document.getElementById("i1howmany").innerHTML = "Input the amount of this item you wish to purchase. <input type='text' id='numiron1'>";
+        document.getElementById("i1howmany").innerHTML += "<br> <button value='Add this many to your cart'>";
     }
 }
