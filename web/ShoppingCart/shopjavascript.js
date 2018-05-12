065d0301204b87e48086975a5417898202d101ad
William Johnson <joh15132@byui.edu>
2018-05-11 19:08:04 -0600

    trying to add html via javascript

--- web/ShoppingCart/shopjavascript.js
+++ web/ShoppingCart/shopjavascript.js
@@ -1,6 +1,14 @@
 function onToNumbers(){
     if(document.getElementById("ironman1").checked == true){
-        document.getElementById("ironman1").value = 1;
-        document.getElementById("testing").innerHTML = document.getElementById("ironman1").value; 
+        
+        //document.getElementById("ironman1").value = 1;
+        //document.getElementById("testing").innerHTML = document.getElementById("ironman1").value; 
+        document.getElementById("")
+
+        //if(document.getElementById("preformance")[x].value == "Duet"){
+            document.getElementById("secondFName").innerHTML = "Input the amount of this item you wish to purchase. <input type='text' id='numiron1'>";
+            //First Name - Second Student <br> <input type='text' id='sfName' name='sfName'>";
+            
+            //}
     }
 }
