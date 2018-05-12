496171a121359779258bec3e1a5f4b441547841d
William Johnson <joh15132@byui.edu>
2018-05-11 19:48:32 -0600

    today if you don't mind

--- web/ShoppingCart/shopjavascript.js
+++ web/ShoppingCart/shopjavascript.js
@@ -1,10 +1,16 @@
-function onToNumbers(){
+function howMany(){
     if(document.getElementById("ironman1").checked == true){
         
         //document.getElementById("ironman1").value = 1;
         //document.getElementById("testing").innerHTML = document.getElementById("ironman1").value; 
 
         document.getElementById("i1howmany").innerHTML = "Input the amount of this item you wish to purchase. <input type='text' id='numiron1'>";
-        document.getElementById("i1howmany").innerHTML += "<br> <input type='button' value='Add this many to your cart'>";
+        document.getElementById("i1howmany").innerHTML += "<br> <input type='button' value='Add this many to your cart' onclick='add()'>";
     }
+}
+
+function add(){
+    document.getElementById("ironman1").value += document.getElementById("numiron1").value;
+    
+    document.getElementById("testing").innerHTML = document.getElementById("ironman1").value; 
 }
