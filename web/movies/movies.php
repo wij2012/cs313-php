be2e370c7feac51acf365af689a72be84f24a647
William Johnson <joh15132@byui.edu>
2018-05-25 13:39:58 -0600

    stupid

--- web/movies/movies.php
+++ web/movies/movies.php
@@ -1,54 +1,48 @@
 <?php
-    try{
-     $dbUrl = getenv('DATABASE_URL');
-
-     $dbopts = parse_url($dbUrl);
-     
-     $dbHost = $dbopts["host"];
-     $dbPort = $dbopts["port"];
-     $dbUser = $dbopts["user"];
-     $dbPassword = $dbopts["pass"];
-     $dbName = ltrim($dbopts["path"],'/');
-     
-     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-     
-     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-
-    catch(PDOException $ex){
-        echo 'Error!' . $ex->getMessage();
-        die();
-    }
+try
+{
+    $dbUrl = getenv('DATABASE_URL');
+    $dbopts = parse_url($dbUrl);
+    
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"],'/');
+    
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
 </head>
 <body>
     <h1>Movies</h1>
-    <ul>
-    <?php
-        $user_rating = $_GET["rating"];
-        
-        $query = "SELECT m.title, m.year, r.code FROM movies m INNER JOIN ratings r ON m.rating_id = r.id WHERE r.code = ':rating';";
-        $statement = $db->prepare($query);
-        $statement.bindvalue(":rating", $user_rating, PDO::PARAM_STR);
-        $statement->execute();
 
-        foreach($statement->fetchall(PDO::FETCH_ASSOC) as $movie){
-            $title = $movie["title"];
-            $year = $movie["year"];
-            $rating = $movie["code"];
-            echo "<li>$title, ($year), $rating </li>";
-        }
-    ?>
+    <ul>
+<?php
+$user_rating = $_GET["rating"];
+$query = "SELECT m.title, m.year, r.code FROM movies m INNER JOIN ratings r ON m.rating_id = r.id WHERE r.code = :rating";
+$statement = $db->prepare($query);
+$statement->bindValue(":rating", $user_rating, PDO::PARAM_STR);
+$statement->execute();
+foreach ($statement->fetchAll(PDO::FETCH_ASSOC) as $movie)
+{
+    $title = $movie["title"];
+    $year = $movie["year"];
+    $rating = $movie["code"];
+    
+    echo "<li>$title ($year) - Rated $rating</li>";
+}
+?>
     </ul>
 
-    
 </body>
 </html>
