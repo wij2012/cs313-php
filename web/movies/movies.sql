b730e5a422bc4ae203bfc5a2ffb72bc45696de5a
William Johnson <joh15132@byui.edu>
2018-05-25 20:54:26 -0600

    crap

--- web/movies/movies.sql
+++ web/movies/movies.sql
@@ -1,7 +1,7 @@
 CREATE TABLE ratings
 (
     id SERIAL PRIMARY KEY,
-    code VARCHAR(10) UNIQUE NOT NULL,
+    code VARCHAR(10) UNIQUE NOT NULL, 
     name VARCHAR(100)
 );
 
@@ -14,23 +14,38 @@ CREATE TABLE movies
 );
 
 INSERT INTO ratings(code, name) VALUES ('G', 'General Audiences');
-INSERT INTO ratings(code, name) VALUES ('PG', 'Parental Guidance Suggested');
-INSERT INTO ratings(code, name) VALUES ('PG-13', 'Parents Strongly Cautioned');
-INSERT INTO ratings(code, name) VALUES ('R', 'Restricted');
-INSERT INTO ratings(code, name) VALUES ('NR', 'Not Rated');
+INSERT INTO ratings(code, name) VALUES
+  ('PG', 'Parental Guidance Suggested'),
+  ('PG-13', 'Parents Strongly Cautioned'),
+  ('R', 'Restricted'),
+  ('NR', 'Not Rated');
 
-INSERT INTO movies(title, year, rating_id) VALUES ('The Incredibles 2',  2018, 2);
-INSERT INTO movies(title, year, rating_id) VALUES ('A New Hope',  1977, (SELECT id FROM ratings WHERE code = 'PG'));
-INSERT INTO movies(title, year, rating_id) VALUES ('Logan', 2017, (SELECT id FROM ratings WHERE code = 'R'));
-INSERT INTO movies(title, year, rating_id) VALUES ('Finding Nemo', 2003, 1);
-INSERT INTO movies(title, year, rating_id) VALUES ('Star Wars: A Christmas Special', NULL, NULL);
-INSERT INTO movies(title, year, rating_id) VALUES ('Star Wars: The Phantom Menace', 1999, NULL);
+INSERT INTO movies(title, year, rating_id) VALUES ('The Incredibles 2', 2018, 2);
+INSERT INTO movies(title, year, rating_id) VALUES ('Star Wars: A New Hope', 1977,
+  (SELECT id FROM ratings WHERE code = 'PG'));
+
+INSERT INTO movies(title, year, rating_id) VALUES ('Star Wars: Christmas Special', NULL,
+  (SELECT id FROM ratings WHERE code = 'NR'));
+
+INSERT INTO movies(title, year, rating_id) VALUES ('Logan', 2017,
+  (SELECT id FROM ratings WHERE code = 'R'));
+
+INSERT INTO movies(title, year, rating_id) VALUES ('Finding Nemo', 2003,
+  (SELECT id FROM ratings WHERE code = 'G'));
 
 SELECT title, year FROM movies WHERE year > 2000;
 SELECT * FROM movies WHERE title LIKE '%Star Wars%';
 
-SELECT * FROM movies m INNER JOIN ratings r 
-ON m.rating_id = r.id;
+SELECT * FROM movies m
+  INNER JOIN ratings r ON m.rating_id = r.id;
 
-SELECT m.title, m.year, r.code FROM movies m INNER JOIN ratings r 
-ON m.rating_id = r.id;
+SELECT m.title, m.year, r.code FROM movies m
+  INNER JOIN ratings r ON m.rating_id = r.id;
+
+SELECT m.title, m.year FROM movies m
+  INNER JOIN ratings r ON m.rating_id = r.id
+  WHERE r.code = 'PG';
+
+SELECT * FROM movies m
+  INNER JOIN movies_actors ma ON m.id = ma.movie_id
+  INNER JOIN actors a ON ma.actor_id = a.id;
