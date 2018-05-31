7311983682b4b3cf2a7e5295b4d18e8aa79d2471
William Johnson <joh15132@byui.edu>
2018-05-31 16:02:54 -0600

    new table column

--- web/project01/chess.php
+++ web/project01/chess.php
@@ -46,7 +46,7 @@
     
         else if($table == 'matches'){
             echo '<div><strong>Matches</strong> <br>';
-            echo '<table><tr><th> Player 1 </th> <th> Player 2 </th> <th> Winner </th> <th> Date Played </th></tr>';
+            echo '<table><tr><th>Match ID Number</th> <th> Player 1 </th> <th> Player 2 </th> <th> Winner </th> <th> Date Played </th></tr>';
             /*foreach ($db->query('SELECT * FROM match;') as $row)
             {
                 echo '<tr><td>' . $row['player1'] . '</td>';
