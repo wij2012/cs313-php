823438cc81448b367c2a06740728233e3cb5b12f
William Johnson <joh15132@byui.edu>
2018-06-09 15:20:27 -0600

    working on addMatch

--- web/project01/addMatch.php
+++ web/project01/addMatch.php
@@ -37,8 +37,11 @@
     $player1 = $_POST["player1"];
     $player2 = $_POST["player2"];
     $winner = $_POST["winner"];
-
-    echo " $player1 - $player2 - $winner";
+    $year = $_POST["year"];
+    $month = $_POST["month"];
+    $day = $_POST["day"];
+    $date = $year . "-" . $month . "-" . $day;
+    echo " $player1 - $player2 - $winner - $date";
 
     //find player ids goes here
     
@@ -79,22 +82,22 @@
     Month 
     <br>
     <select  name="month">
-        <option value="January"> January </option>
-        <option value="February"> February </option>
-        <option value="March"> March </option>
-        <option value="April"> April </option>
-        <option value="May"> May </option>
-        <option value="June"> June </option>
-        <option value="July"> July </option>
-        <option value="August"> August </option>
-        <option value="September"> September </option>
-        <option value="October"> October </option>
-        <option value="November"> November </option>
-        <option value="December"> December </option>
+        <option value="01"> January </option>
+        <option value="02"> February </option>
+        <option value="03"> March </option>
+        <option value="04"> April </option>
+        <option value="05"> May </option>
+        <option value="06"> June </option>
+        <option value="07"> July </option>
+        <option value="08"> August </option>
+        <option value="09"> September </option>
+        <option value="10"> October </option>
+        <option value="11"> November </option>
+        <option value="12"> December </option>
     </select>
     Year
     <br>
-    <select name="" name="month">
+    <select name="year">
         <option value="2016"> 2016 </option>
         <option value="2017"> 2017 </option>
         <option value="2018"> 2018 </option>
