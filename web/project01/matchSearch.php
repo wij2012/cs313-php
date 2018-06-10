f80b4a08cfc9b6ea69ec4604b839c44aa2c98ac1
William Johnson <joh15132@byui.edu>
2018-06-09 20:36:38 -0600

    debugging recomences more

--- web/project01/matchSearch.php
+++ web/project01/matchSearch.php
@@ -54,7 +54,6 @@
         $statement->bindValue(":id", $id, PDO::PARAM_INT);
         $statement->execute();
 
-        echo "date played " . $_POST["datePlayed"];
         echo "<div><strong> Record of searched match </strong>";
         echo "<table><tr><th>Match ID #</th> <th> Player 1 </th> <th> Player 2 </th> <th> Winner </th> <th> Date Played </th></tr>";
         foreach($statement->fetchAll() as $match){
@@ -65,7 +64,7 @@
             echo "<td>" . $match["datePlayed"] . "</td></tr>";
         }
         echo "</table> </div> <br><br>";
-
+        echo "date played " . $match["datePlayed"];
 
         //print comments on searched match
         $query = "SELECT players.name
